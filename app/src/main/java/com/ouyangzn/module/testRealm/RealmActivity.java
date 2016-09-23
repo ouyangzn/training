8f7847ce500c04b4b9ea489effe695776a57f18b
ouyangzn <ouyangzn@163.com>
2016-09-23 18:07:42 +0800

    reaml数据库学习--测试大数量级别（10W+）数据的插入与查询

--- app/src/main/java/com/ouyangzn/module/testRealm/RealmActivity.java
+++ app/src/main/java/com/ouyangzn/module/testRealm/RealmActivity.java
@@ -16,10 +16,12 @@ import com.ouyangzn.module.testRealm.adapter.PersonRecyclerAdapter;
 import com.ouyangzn.module.testRealm.bean.Dog;
 import com.ouyangzn.module.testRealm.bean.Person;
 import com.ouyangzn.utils.Log;
+import io.realm.Case;
 import io.realm.Realm;
 import io.realm.RealmChangeListener;
 import io.realm.RealmResults;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
@@ -31,6 +33,7 @@ public class RealmActivity extends BaseActivity {
   @BindView(R.id.btn_delete_dog) Button mDeleteBtn;
   @BindView(R.id.btn_query_dog) Button mQueryBtn;
   @BindView(R.id.btn_update_dog) Button mUpdateBtn;
+  long beginTime;
   private Realm mRealm;
   private DogRecyclerAdapter mDogAdapter;
   private PersonRecyclerAdapter mPersonAdapter;
@@ -93,19 +96,39 @@ public class RealmActivity extends BaseActivity {
   }
 
   private void deletePerson() {
-
+    //mRealm.delete(Person.class);
+    mRealm.executeTransactionAsync(new Realm.Transaction() {
+      @Override public void execute(Realm realm) {
+        boolean result =
+            realm.where(Person.class).contains("name", "1").findAll().deleteAllFromRealm();
+        Log.d(TAG, result ? "----------删除成功--------" : "-------删除失败---------");
+      }
+    });
   }
 
   private void insertPerson() {
     mRealm.executeTransactionAsync(new Realm.Transaction() {
       @Override public void execute(Realm realm) {
-        Person person = new Person();
-        person.setName("Person" + new Random(10).nextInt(Integer.MAX_VALUE));
-        person.setId(UUID.randomUUID().getLeastSignificantBits());
-        realm.copyToRealm(person);
+        //Person person = new Person();
+        //person.setName("person");
+        //person.setId(UUID.randomUUID().getLeastSignificantBits());
+        //realm.copyToRealm(person);
+        long time = System.currentTimeMillis();
+        ArrayList<Person> list = new ArrayList<>();
+        Person person;
+        for (int i = 1; i < 100000; i++) {
+          person = new Person();
+          person.setName("person_" + i);
+          person.setId(UUID.randomUUID().getLeastSignificantBits());
+          list.add(person);
+        }
+        Log.d(TAG, "----------制造数据耗时：" + (System.currentTimeMillis() - time));
+        beginTime = System.nanoTime();
+        realm.copyToRealm(list);
       }
     }, new Realm.Transaction.OnSuccess() {
       @Override public void onSuccess() {
+        Log.d(TAG, "----------插入数据完整流程耗时：" + (System.currentTimeMillis() - beginTime));
         toast("数据插入成功");
       }
     }, new Realm.Transaction.OnError() {
@@ -117,14 +140,33 @@ public class RealmActivity extends BaseActivity {
   }
 
   private void queryPerson() {
-    final RealmResults<Person> results = mRealm.where(Person.class).findAllAsync();
+    RealmResults<Person> results =
+        // 查询的表
+        mRealm.where(Person.class)
+            // name like '%person%'
+            .contains("name", "person", Case.INSENSITIVE)
+            // 异步查询所有数据
+            .findAllAsync();
     results.addChangeListener(new RealmChangeListener<RealmResults<Person>>() {
       @Override public void onChange(RealmResults<Person> element) {
-        // RealmResults不支持clear、add之类的操作
-        ArrayList<Person> personList = new ArrayList<>(results.size());
-        personList.addAll(results);
-        Log.d(TAG, "----------queryPerson.result = " + personList);
-        mPersonAdapter.resetData(personList);
+        // RealmResults不支持clear、addAll之类的操作
+        System.gc();
+        long total = Runtime.getRuntime().totalMemory(); // byte
+        long m1 = Runtime.getRuntime().freeMemory();
+        Log.d(TAG, "----------before,内存占用:" + (total - m1));
+        List<Person> personList;
+        if (element.size() > 30) {
+          personList = new ArrayList<>(30);
+          personList.addAll(element.subList(0, 30));
+        } else {
+          personList = new ArrayList<>(element.size());
+          personList.addAll(element);
+        }
+        long total1 = Runtime.getRuntime().totalMemory();
+        long m2 = Runtime.getRuntime().freeMemory();
+        Log.d(TAG, "----------after,内存占用:" + (total1 - m2));
+        Log.d(TAG, "----------queryPerson.result.size = " + personList.size());
+        mPersonAdapter.resetData(personList.size() > 30 ? personList.subList(0, 30) : personList);
       }
     });
   }
